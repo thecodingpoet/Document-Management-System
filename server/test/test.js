26f916a7969784d55f9abd45aa147a0ede3df464
andela-oduyile <oluwatomi.duyile@andela.com>
2017-04-07 15:34:51 +0100

    chore(integrate-code-coverage) modified test [Start #142496269]

--- server/test/test.js
+++ server/test/test.js
@@ -1,9 +1,11 @@
+import isEven from './func'
+
 const assert = require('assert');
 
 describe('Array', function() {
   describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3].indexOf(4));
+    it('should return true for n is equal to 4', function() {
+      assert.equal(true, isEven(4));
     });
   });
 });

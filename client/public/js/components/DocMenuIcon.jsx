01627c64bd537f9122397ed22e00d01b179bde5c
andela-oduyile <oluwatomi.duyile@andela.com>
2017-05-05 13:59:51 +0100

    add document icon menu

---
+++ client/public/js/components/DocMenuIcon.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+/**
+ * Simple Icon Menus demonstrating some of the layouts possible using the `anchorOrigin` and
+ * `targetOrigin` properties.
+ */
+/* const IconsMenu = () => (
+  <div className="cardzIcon">
+    <MuiThemeProvider >
+      <IconMenu
+        iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+        anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+        targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+
+      >
+        <MenuItem primaryText="Edit" href="editDocument" />
+        <MenuItem primaryText="Delete" />
+      </IconMenu>
+    </MuiThemeProvider >
+  </div>
+);*/
+
+export default class IconsMenu extends Component {
+  componentDidMount() {
+    $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
+    }
+   );
+  }
+
+  render() {
+    return (
+      <div className="cardzIcon">
+        <a className="dropdown-button btn" href="#whatever" data-activates="dropdown1">ic</a>
+        <ul id="dropdown1" className="dropdown-content">
+          <li><a href="#!">Edit</a></li>
+          <li><a href="#!">Delete</a></li>
+        </ul>
+      </div>
+    );
+  }
+}

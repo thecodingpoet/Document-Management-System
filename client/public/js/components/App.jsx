c662d937b8b08441bc7b17b6bb80c09194a831b9
andela-oduyile <oluwatomi.duyile@andela.com>
2017-05-05 13:58:57 +0100

    add App.jsx

---
+++ client/public/js/components/App.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Home.jsx';
+import Login from './Login.jsx';
+import Signup from './Signup.jsx';
+import Dashboard from './Dashboard.jsx';
+import Footer from './Footer.jsx';
+import SideNav from './SideNav.jsx';
+import Nav from './Nav.jsx';
+import MyDocuments from './MyDocuments.jsx';
+
+require('../../scss/style.scss');
+
+export default class App extends Component {
+  render() {
+    if (window.localStorage.getItem('token')) {
+      return (
+        <div>
+          <div className="content">
+            <SideNav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/document" component={MyDocuments} />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      );
+    }
+    return (
+      <div>
+        <div className="content">
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}

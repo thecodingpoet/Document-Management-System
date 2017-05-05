02a9a65aa2f371140c7bb529805a2f39a20a533b
andela-oduyile <oluwatomi.duyile@andela.com>
2017-05-05 14:18:18 +0100

    add login validation

---
+++ client/public/js/validations/login.js
@@ -0,0 +1,19 @@
+import validator from 'validator';
+import isEmpty from 'lodash/isEmpty';
+
+export default function validateInput(data) {
+  const errors = {};
+
+  if (validator.isEmpty(data.email)) {
+    errors.email = 'This Field is required';
+  }
+
+  if (validator.isEmpty(data.password)) {
+    errors.password = 'This Field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+}

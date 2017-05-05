36cb2710a96c1108f66ea6c20299892cfc8c0a2c
andela-oduyile <oluwatomi.duyile@andela.com>
2017-05-05 14:26:52 +0100

    add swagger api docs

---
+++ server/routes/swagger.js
@@ -0,0 +1,23 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+  // swagger definition
+const swaggerDefinition = {
+  info: {
+    title: 'Document Management System API',
+    version: '1.0.0',
+    description: 'API documentation for to create, manage and edit documents',
+  }
+};
+
+// options for the swagger docs
+const options = {
+  // import swaggerDefinitions
+  swaggerDefinition,
+  // path to the API docs
+  apis: ['./server/routes/document.js', './server/routes/user.js', './server/routes/role.js'],
+};
+
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
+export default swaggerSpec;

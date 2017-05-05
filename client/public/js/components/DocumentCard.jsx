bcc8776b6c08a401a93ad46f44ccd103e6a17fec
andela-oduyile <oluwatomi.duyile@andela.com>
2017-05-05 14:01:18 +0100

    add document card

---
+++ client/public/js/components/DocumentCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import DocMenuIcon from './DocMenuIcon.jsx';
+
+require('../../scss/style.scss');
+
+export default function DocumentCard({ doc }) {
+  return (
+    <div className="col s12 m4">
+      <div className="card grey lighten-3 ">
+        <div className="card-content white-text cardz">
+          <span className="card-title"><b>{doc.title}</b></span>
+          <p id="content" style={{ color: 'black' }} dangerouslySetInnerHTML={{ __html: doc.content }} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+DocumentCard.propTypes = {
+  doc: React.PropTypes.object.isRequired
+};

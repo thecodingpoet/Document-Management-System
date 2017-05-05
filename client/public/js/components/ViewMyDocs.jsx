4af058a77dc0d770e4adb0ebf22cc97e3286899f
andela-oduyile <oluwatomi.duyile@andela.com>
2017-05-05 14:20:53 +0100

    add viewmydocs component

---
+++ client/public/js/components/ViewMyDocs.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import MyDocsList from './MyDocsList.jsx';
+import { fetchPublicDocs } from '../actions/documents.js';
+
+class ViewMyDocuments extends Component {
+  componentDidMount() {
+    this.props.fetchPublicDocs();
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div id="public" className="col s12">
+          <MyDocsList docs={this.props.docs} />
+        </div>
+      </div>
+    );
+  }
+}
+
+ViewMyDocuments.propTypes = {
+  fetchPublicDocs: React.PropTypes.func.isRequired,
+  docs: React.PropTypes.array.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    docs: state.documents
+  };
+}
+
+export default connect(mapStateToProps, { fetchPublicDocs })(ViewMyDocuments);

edb46ff70ca91e3d33fd1b545a969ff90483bbbd
andela-oduyile <oluwatomi.duyile@andela.com>
2017-05-05 14:16:23 +0100

    add my documents component

---
+++ client/public/js/components/MyDocuments.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import AddModal from './AddModal.jsx';
+import EditModal from './EditModal.jsx';
+import AddBtn from './AddButton.jsx';
+import MyDocs from './MyDocuments.jsx';
+import ViewMyDocs from './ViewMyDocs.jsx';
+import EditDocument from './EditDocument.jsx';
+
+class MyDocuments extends Component {
+  componentDidMount() {
+    $('.modal').modal();
+    $('select').material_select();
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <AddModal />
+        <EditModal />
+        
+        <ViewMyDocs />
+        
+        <AddBtn />
+        
+      </div>
+    );
+  }
+
+}
+
+export default MyDocuments;

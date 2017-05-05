293a0aba2650462381a6a30a4700c70cdfe6b7a4
andela-oduyile <oluwatomi.duyile@andela.com>
2017-05-05 14:17:58 +0100

    add view document component

---
+++ client/public/js/components/ViewDocuments.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PublicDocs from './PublicDocuments.jsx';
+import PrivateDocs from './PrivateDocuments.jsx';
+import RoleDocs from './RoleDocuments.jsx';
+
+export default class ViewDocuments extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col s12">
+          <ul id="tabs-swipe-demo" className="tabs">
+            <li className="tab col s3"><a className="active" href="#public">Public</a></li>
+            <li className="tab col s3"><a href="#private">Private</a></li>
+            <li className="tab col s3"><a href="#role">Role</a></li>
+          </ul>
+        </div>
+
+        <div id="public" className="col s12"><PublicDocs /></div>
+        <div id="private" className="col s12"><PrivateDocs /></div>
+        <div id="role" className="col s12"><RoleDocs /></div>
+      </div>
+
+    );
+  }
+}
+

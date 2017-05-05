0145950bcfa4a2ffe50fec1659056bd3a56fa615
andela-oduyile <oluwatomi.duyile@andela.com>
2017-05-05 14:14:57 +0100

    add public document list component

---
+++ client/public/js/components/PublicDocsList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import DocumentCard from './DocumentCard.jsx';
+
+export default function PublicDocsList({ docs }) {
+  const emptyMessage = (
+    <div className="container">
+      <p className="right-align">There are no Public Documents yet</p>
+    </div>
+  ); 
+
+  const publicDocsList = docs.filter(doc => doc.access === 'public');
+
+  const docsList = (
+    <div className="container" id="docList">
+      <div className="row">
+        { publicDocsList.map(doc => <DocumentCard doc={doc} key={doc.id} />) }
+      </div>
+    </div>
+  );
+
+  return (
+    <div>
+      { publicDocsList.length === 0 ? emptyMessage : docsList }
+    </div>
+  );
+}
+
+PublicDocsList.propTypes = {
+  docs: React.PropTypes.array.isRequired
+};

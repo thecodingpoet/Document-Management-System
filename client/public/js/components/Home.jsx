09130226428f49e43ae5517195edf1645672b2e9
andela-oduyile <oluwatomi.duyile@andela.com>
2017-05-05 14:11:51 +0100

    add home page component

---
+++ client/public/js/components/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import About from './About.jsx';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <img src={require('../../images/Banner3.jpg')}  alt=" landing page banner" id="banner" />
+        <About />
+      </div>
+
+    );
+  }
+}
+
+export default Home;

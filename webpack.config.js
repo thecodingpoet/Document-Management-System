0b76fbe63ceb99ef5e4b50433c70fbc0c4f9605d
andela-oduyile <oluwatomi.duyile@andela.com>
2017-05-05 21:37:42 +0100

    fix heroku build

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
-        // query: {
-        //   presets: ['react']
-        // }
+        exclude: /node_modules/
       },
       {
         test: /\.scss$/,

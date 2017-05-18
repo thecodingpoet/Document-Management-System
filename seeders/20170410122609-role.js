bac79162e40da97c2b3c3840bdf4f9d22e97dfb7
andela-oduyile <oluwatomi.duyile@andela.com>
2017-05-18 11:59:43 +0100

    remove unneccessary comments

--- seeders/20170410122609-role.js
+++ seeders/20170410122609-role.js
@@ -1,15 +1,6 @@
 
 module.exports = {
   up(queryInterface) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
     return queryInterface.bulkInsert('Role', [
       {
         title: 'admin',
@@ -27,12 +18,6 @@ module.exports = {
   },
 
   down(queryInterface) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
     return queryInterface.bulkDelete('Role', null, {});
   }
 };
